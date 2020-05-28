f758a461b5f436cb866696edd68eab326568713b
Akashgayakwad <akashgayakwad123@gmail.com>
2020-05-28 15:10:52 +0530

    cities and states api added

--- user/urls.py
+++ user/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('verify_otp/', views.verify_otp),
     path('get_profile/',views.get_profile),
     path('update_profile/',views.update_profile),
+    path('get_state_list/',views.get_state_list),
+    path('get_city_list/',views.get_city_list),
 ]
