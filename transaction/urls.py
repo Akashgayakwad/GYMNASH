b04ebf602ed9b25440ad0009018af05c4f89e054
Akashgayakwad <akashgayakwad123@gmail.com>
2020-08-07 23:14:26 +0530

    transaction api corrected

--- transaction/urls.py
+++ transaction/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('get_monthly_price/',views.get_monthly_price),
     path('create_order/',views.create_order),
     path('confirm_payment/',views.confirm_payment),
-    path('payment_failure',views.payment_failure),
+    path('payment_failure/',views.payment_failure),
     path('get_expiry/',views.get_expiry)
 ]
